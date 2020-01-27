4d0c7e44f5c05f63244d2edd42ea5309911ebf71
Vincent Costa <costa.vincent132@gmail.com>
2020-01-27 15:03:30 -0800

    run has a cleaner function but slower runtime

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.reg = [0b00000000] * register_count
         self.ram = [0b00000000] * calloc_size
         self.instruction = 0
+        self.active = False
 
     def load(self):
         """Load a program into memory."""
@@ -34,7 +35,7 @@ class CPU:
 
     def ram_read(self, position):
         return self.ram[position]
-        
+
     def ram_write(self, position, binary_value):
         if position < 0 or position >= len(self.ram):
             print("SEG FAULT NO POS")
@@ -58,12 +59,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.instruction,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.instruction),
+            self.ram_read(self.instruction + 1),
+            self.ram_read(self.instruction + 2)
         ), end='')
 
         for i in range(8):
@@ -71,17 +72,25 @@ class CPU:
 
         print()
 
-    def run(self, instruction_start = 0):
+    def run(self, instruction_start = 0, legacy = False):
         """Run the CPU."""
-        self.instruction = instruction_start
-        active = True
+        self.active = True
 
-        while active:
+        if legacy is True:
+            self.run_legacy(instruction_start)
+        else:
+            self.run_hot(instruction_start)
+
+    def run_legacy(self, instruction_start = 0):
+        self.instruction = instruction_start
+        self.active = True
+
+        while self.active:
             instruc_val = self.ram[self.instruction]
 
             if instruc_val == 0b00000001: # HLT: HALT
                 # self.trace()
-                active = False
+                self.active = False
             elif instruc_val == 0b01000111: # PRN (R#): Print register value
                 register = self.ram[self.instruction + 1]
 
@@ -100,5 +109,46 @@ class CPU:
                 self.reg[register] = value
 
                 self.instruction += 3
+
+        print("HLT")
+
+    def run_hot(self, instruction_start = 0):
+        self.instruction = instruction_start
+        self.active = True
+
+        def HLT():
+            self.trace()
+            self.active = False
+
+        def PRN():
+            register = self.ram[self.instruction + 1]
+
+            print(self.reg[register])
+
+            self.instruction += 2
+
+        def LDI():
+            register = self.ram[self.instruction + 1]
+            value = self.ram[self.instruction + 2]
+
+            if register < 0 or register >= len(self.reg):
+                self.trace()
+                print("SEG FAULT NO REG")
+                sys.exit(1)
+
+            self.reg[register] = value
+
+            self.instruction += 3
+
+        instruction_map = {
+            0b00000001: HLT,
+            0b01000111: PRN,
+            0b10000010: LDI
+        }
+
+        while self.active:
+            instruction_val = self.ram[self.instruction]
+
+            instruction_map[instruction_val]()
 
         print("HLT")
