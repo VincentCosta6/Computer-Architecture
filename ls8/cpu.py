d545cae8d8309bba6a1a0bd89696d85caef100fb
Vincent Costa <costa.vincent132@gmail.com>
2020-01-27 23:25:12 -0800

    Finish step 11

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,33 +5,64 @@ import sys
 class CPU:
     """Main CPU class."""
 
-    def __init__(self, register_count = 8, calloc_size = 10000):
+    def __init__(self, register_count = 8, calloc_size = 10000, DEBUG = False):
         """Construct a new CPU."""
         self.reg = [0b00000000] * register_count
         self.ram = [0b00000000] * calloc_size
+
+        self.instruction_map = {
+            0b00000001: self.HLT,
+            0b01000111: self.PRN,
+
+            0b10000010: self.LDI,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+
+            0b01010000: self.CALL,
+            0b00010001: self.RET,
+
+            0b10100000: self.ADD,
+            0b10100010: self.MUL,
+        }
+
+        self.ram_top = calloc_size - 1
+        self.stack_last = 0
         self.instruction = 0
+        self.program_end = 0
         self.active = False
+        self.DEBUG = DEBUG
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
-
         address = 0
+        break_keys = { ' ', '\n', '#' }
 
-        # For now, we've just hardcoded a program:
+        def parse_line(line):
+            if len(line) == 0 or line[0] in break_keys:
+                return [False]
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+            code = ''
+            
+            for c in line:
+                if c in break_keys:
+                    break
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                code += c
+
+            if len(code) > 0 and len(code) != 8:
+                raise Exception("Program load failed, instruction was not 8 bits")
+
+            return [True, int(code, base=2)]
+
+        with open(file_name, "r") as file:
+            for line in file:
+                return_values = parse_line(line)
+
+                if return_values[0] == True:
+                    self.ram[address] = return_values[1]
+                    address += 1
+
+        self.program_end = address
 
     def ram_read(self, position):
         return self.ram[position]
@@ -48,9 +79,125 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB": 
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
+    def HLT(self):
+        self.active = False
+
+    def PRN(self):
+        register = self.ram[self.instruction + 1]
+
+        print(f"PRINT: LINE: {self.instruction}, REG: {register}, VAL: {self.reg[register]}")
+
+        self.instruction += 2
+
+    def LDI(self):
+        register = self.ram[self.instruction + 1]
+        value = self.ram[self.instruction + 2]
+
+        if register < 0 or register >= len(self.reg):
+            self.trace()
+            print("SEG FAULT NO REG")
+            sys.exit(1)
+
+        if self.DEBUG:
+            print(f"LOAD: LINE: {self.instruction}, REG: {register}, VAL: {value}")
+
+        self.reg[register] = value
+
+        self.instruction += 3
+
+    def PUSH(self):
+        register = self.ram[self.instruction + 1]
+        
+        if self.ram_top - self.stack_last <= self.program_end + 1:
+            raise Exception("Stack overflow")
+
+        self.ram[self.ram_top - self.stack_last] = self.reg[register]
+
+        if self.DEBUG is True:
+            print(f"PUSH: reg: {register}, val: {self.reg[register]}")
+
+            for i in range(self.stack_last + 1):
+                print(f"  {self.ram_top - i}: {self.ram[self.ram_top - i]}")
+
+        self.stack_last += 1
+        self.instruction += 2
+
+    def POP(self):
+        register = self.ram[self.instruction + 1]
+
+        if self.stack_last == 0:
+            raise Exception("Stack underflow")
+
+        self.stack_last -= 1
+
+        if self.DEBUG:
+            print(f"POP: REG: {register}, ADDR: {self.ram_top - self.stack_last}, VAL: {self.ram[self.ram_top - self.stack_last]}")
+
+        self.reg[register] = self.ram[self.ram_top - self.stack_last]
+        self.ram[self.ram_top - self.stack_last] = 0b00000000
+
+        self.instruction += 2
+
+    def CALL(self):
+        register = self.ram[self.instruction + 1]
+        register_value = self.reg[register]
+
+        if self.DEBUG:
+            print("PUSH BY CALL")
+
+        self.ram[self.ram_top - self.stack_last] = self.instruction
+
+        if self.DEBUG is True:
+            print(f"PUSH: VAL: {self.instruction}")
+
+            for i in range(self.stack_last + 1):
+                print(f"  {self.ram_top - i}: {self.ram[self.ram_top - i]}")
+
+        self.stack_last += 1
+
+        if self.DEBUG:
+            print(f"CALL: REG: {register}, VAL: {register_value}")
+
+        self.instruction = register_value
+
+    def RET(self):
+        self.stack_last -= 1
+
+        return_address = self.ram[self.ram_top - self.stack_last]
+
+        if self.DEBUG:
+            print(f"RET: ADDR: {return_address}")
+
+        self.ram[self.ram_top - self.stack_last] = 0b00000000
+        self.instruction = return_address + 2
+
+    def ADD(self):
+        register_a = self.ram[self.instruction + 1]
+        register_b = self.ram[self.instruction + 2]
+
+        if self.DEBUG:
+            print(f"ADD: LINE: {self.instruction}, REG_A: {register_a}, REG_B: {register_b}, RES: {self.reg[register_a] + self.reg[register_b]}")
+
+        self.alu("ADD", register_a, register_b)
+
+        self.instruction += 3
+
+    def MUL(self):
+        register_a = self.ram[self.instruction + 1]
+        register_b = self.ram[self.instruction + 2]
+
+        self.alu("MUL", register_a, register_b)
+
+        self.instruction += 3
 
     def trace(self):
         """
@@ -70,85 +217,17 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-        print()
-
-    def run(self, instruction_start = 0, legacy = False):
+    def run(self, instruction_start = 0):
         """Run the CPU."""
-        self.active = True
-
-        if legacy is True:
-            self.run_legacy(instruction_start)
-        else:
-            self.run_hot(instruction_start)
-
-    def run_legacy(self, instruction_start = 0):
-        self.instruction = instruction_start
-        self.active = True
-
-        while self.active:
-            instruc_val = self.ram[self.instruction]
-
-            if instruc_val == 0b00000001: # HLT: HALT
-                # self.trace()
-                self.active = False
-            elif instruc_val == 0b01000111: # PRN (R#): Print register value
-                register = self.ram[self.instruction + 1]
-
-                print(self.reg[register])
-
-                self.instruction += 2
-            elif instruc_val == 0b10000010: # LDI (R#, VAL): Load immediate register with value
-                register = self.ram[self.instruction + 1]
-                value = self.ram[self.instruction + 2]
-
-                if register < 0 or register >= len(self.reg):
-                    # self.trace()
-                    print("SEG FAULT NO REG")
-                    sys.exit(1)
-
-                self.reg[register] = value
-
-                self.instruction += 3
-
-        print("HLT")
+        self.run_hot(instruction_start)
 
     def run_hot(self, instruction_start = 0):
         self.instruction = instruction_start
         self.active = True
 
-        def HLT():
-            self.trace()
-            self.active = False
-
-        def PRN():
-            register = self.ram[self.instruction + 1]
-
-            print(self.reg[register])
-
-            self.instruction += 2
-
-        def LDI():
-            register = self.ram[self.instruction + 1]
-            value = self.ram[self.instruction + 2]
-
-            if register < 0 or register >= len(self.reg):
-                self.trace()
-                print("SEG FAULT NO REG")
-                sys.exit(1)
-
-            self.reg[register] = value
-
-            self.instruction += 3
-
-        instruction_map = {
-            0b00000001: HLT,
-            0b01000111: PRN,
-            0b10000010: LDI
-        }
-
         while self.active:
             instruction_val = self.ram[self.instruction]
 
-            instruction_map[instruction_val]()
+            self.instruction_map[instruction_val]()
 
         print("HLT")
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,7 @@
 import sys
 from cpu import *
 
-cpu = CPU(10000, 8)
+cpu = CPU(8, 10000, False)
 
-cpu.load()
-cpu.run()
+cpu.load(sys.argv[1])
+cpu.run_hot()
